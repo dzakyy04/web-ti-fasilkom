b3336713854ddd8203a09365a5d36d02c79d3a40
dzakyy04 <dzakylinggau@gmail.com>
2024-07-04 22:11:09 +0700

    style(artcile): Update all articles page layout

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -1,64 +1,78 @@
 @extends('admin.layouts.app')
 
+@push('css')
+    <style>
+        .thumbnail-image {
+            height: 200px;
+            object-fit: cover;
+            width: 100%;
+        }
+    </style>
+@endpush
+
+@push('js')
+    <script src="{{ asset('assets/js/libs/datatable-btns.js?ver=3.0.3') }}"></script>
+    <script src="{{ asset('assets/js/example-toastr.js?ver=3.0.3') }}"></script>
+    @if (session()->has('success'))
+        <script>
+            let message = @json(session('success'));
+            NioApp.Toast(`<h5>Berhasil</h5><p>${message}</p>`, 'success', {
+                position: 'top-right',
+            });
+        </script>
+    @endif
+    <script>
+        $(document).ready(function() {
+            const datatableWrap = $(".datatable-wrap");
+            const wrappingDiv = $("<div>").addClass("w-100").css("overflow-x", "scroll");
+            datatableWrap.children().appendTo(wrappingDiv);
+            datatableWrap.append(wrappingDiv);
+        });
+    </script>
+@endpush
+
 @section('content')
     <div class="col-md-12">
         <div class="nk-block nk-block-lg">
-            <div class="nk-block-head">
+            <div class="nk-block-between">
                 <div class="nk-block-head-content">
-                    <h4 class="nk-block-title mb-2">Berita</h4>
+                    <h3 class="nk-block-title page-title">Berita</h3>
+                </div>
+                <div class="nk-block-head-content">
+                    <a href="{{ route('articles.create') }}" class="btn btn-primary">
+                        <em class="icon ni ni-plus me-1"></em>Tambah Berita</span>
+                    </a>
                 </div>
             </div>
-            <div class="card card-bordered card-preview">
-                <div class="card-inner">
-                    <div class="d-flex">
-                        <a href="{{ route('articles.create') }}" class="btn btn-primary mb-2 me-2">
-                            <em class="icon ni ni-plus me-1"></em>Tambah Berita</span>
-                        </a>
-
+            <div class="row">
+                @foreach ($articles as $index => $article)
+                    <div class="col-12 col-sm-6 col-md-4 mt-3">
+                        <div class="card card-bordered">
+                            <img src="{{ Storage::url($article->thumbnail) }}" class="card-img-top thumbnail-image"
+                                alt="">
+                            <div class="card-inner">
+                                <h5 class="card-title">{{ $article->title }}</h5>
+                                <p class="card-text">{{ $article->content }}</p>
+                                <a href="#" class="btn btn-primary d-flex justify-content-center">Lihat
+                                    Berita</a>
+                            </div>
+                            <div
+                                class="card-footer border-top text-muted d-flex justify-content-between align-items-center">
+                                <dev>{{ \Carbon\Carbon::parse($article->updated_at)->translatedFormat('d F Y') }}</dev>
+                                <div class="dropdown">
+                                    <a class="text-soft dropdown-toggle btn btn-icon btn-trigger"
+                                        data-bs-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
+                                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-xs">
+                                        <ul class="link-list-plain">
+                                            <li><a href="#">Edit</a></li>
+                                            <li><a href="#">Hapus</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <table
-                        class="datatable-init nk-tb-list nk-tb-ulist table table-hover table-bordered table-responsive-md"
-                        data-auto-responsive="false">
-                        <thead>
-                            <tr class="table-light nk-tb-item nk-tb-head">
-                                <th class="text-nowrap text-center align-middle">No</th>
-                                <th class="text-nowrap text-center align-middle">Judul</th>
-                                </th>
-                                <th class="text-nowrap text-center align-middle">Thumbnail
-                                <th class="text-nowrap text-center align-middle">Terakhir Diubah
-                                </th>
-                                <th class="text-nowrap text-center no-export align-middle">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($articles as $index => $article)
-                                <tr class="text-center align-middle">
-                                    <td>{{ $index + 1 }}</td>
-                                    <td>{{ $article->title }}</td>
-                                    <td>
-                                        <img src="{{ Storage::url($article->thumbnail) }}" class="img-fluid rounded"
-                                            style="width: 150px">
-                                    </td>
-                                    <td>{{ $article->updated_at }}</td>
-
-                                    <td class="text-nowrap justify-content-center align-items-center text-center">
-                                        <a href="" class="btn btn-primary btn-xs rounded-pill">
-                                            <em class="ni ni-eye"></em>
-                                        </a>
-                                        <a href="" class="btn btn-warning btn-xs rounded-pill">
-                                            <em class="ni ni-edit"></em>
-                                        </a>
-                                        <button class="btn btn-danger btn-xs rounded-pill" data-bs-toggle="modal"
-                                            data-bs-target="#deleteArticleModal" data-slug="{{ $article->slug }}"
-                                            data-title="{{ $article->title }}">
-                                            <em class="ni ni-trash"></em>
-                                        </button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
