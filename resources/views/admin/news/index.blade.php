9405e2b17a7118496cd8e78e9b8918155e6c6f1e
imancrrzii <imancarrazi777@gmail.com>
2024-07-13 23:19:19 +0700

    feat(articles): add search feature on news

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -23,15 +23,22 @@
     @endif
     <script>
         $(document).ready(function() {
-            const datatableWrap = $(".datatable-wrap");
-            const wrappingDiv = $("<div>").addClass("w-100").css("overflow-x", "scroll");
-            datatableWrap.children().appendTo(wrappingDiv);
-            datatableWrap.append(wrappingDiv);
-
-            $('.delete-link').click(function(event) {
-                event.preventDefault();
-                $('.delete-form').submit();
+            $('#search-input').on('input', function() {
+                performSearch();
             });
+
+            function performSearch() {
+                const query = $('#search-input').val().toLowerCase();
+                $('.news-item').each(function() {
+                    const title = $(this).find('.news-title').text().toLowerCase();
+                    const description = $(this).find('.news-description').text().toLowerCase();
+                    if (title.includes(query) || description.includes(query)) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+            }
         });
     </script>
 @endpush
@@ -41,39 +48,42 @@
         <div class="nk-block nk-block-lg">
             <div class="nk-block-between">
                 <div class="nk-block-head-content">
-                    <h3 class="nk-block-title page-title">Berita</h3>
+                    <h3 class="nk-block-title page-title">{{ $title }}</h3>
                 </div>
                 <div class="nk-block-head-content">
                     <a href="{{ route('news.create') }}" class="btn btn-primary">
-                        <em class="icon ni ni-plus me-1"></em>Tambah Berita</span>
-                    </a>
+                        <em class="icon ni ni-plus me-1"></em>Tambah Berita</a>
                 </div>
             </div>
-            <div class="row">
+            <div class="nk-block-head nk-block-head-sm col-md-12">
+                <form id="search-form" method="GET" action="{{ route('news') }}" class="mt-2 row g-2">
+                    <div class="col-md-3">
+                        <input type="text" id="search-input" name="search" class="form-control" placeholder="Ketik untuk mencari" value="{{ request()->get('search') }}">
+                    </div>
+                </form>
+            </div>
+            <div id="news-content" class="row">
                 @foreach ($news as $index => $newsItem)
-                    <div class="col-12 col-sm-6 col-md-4 mt-3">
+                    <div class="col-12 col-sm-6 col-md-4 mt-3 news-item">
                         <div class="card card-bordered">
-                            <img src="{{ Storage::url($newsItem->thumbnail) }}" class="card-img-top thumbnail-image"
-                                alt="">
+                            <img src="{{ Storage::url($newsItem->thumbnail) }}" class="card-img-top thumbnail-image" alt="">
                             <div class="card-inner">
-                                <h5 class="card-title">{{ $newsItem->title }}</h5>
-                                <p class="card-text">{{ $newsItem->description }}</p>
-                                <a href="#" class="btn btn-primary d-flex justify-content-center">Lihat
-                                    Berita</a>
+                                <h5 class="card-title news-title">{{ $newsItem->title }}</h5>
+                                <p class="card-text news-description">{{ $newsItem->description }}</p>
+                                <a href="#" class="btn btn-primary d-flex justify-content-center">Lihat Berita</a>
                             </div>
-                            <div
-                                class="card-footer border-top text-muted d-flex justify-content-between align-items-center">
-                                <dev>{{ \Carbon\Carbon::parse($newsItem->updated_at)->translatedFormat('d F Y') }}</dev>
+                            <div class="card-footer border-top text-muted d-flex justify-content-between align-items-center">
+                                <div>{{ \Carbon\Carbon::parse($newsItem->updated_at)->translatedFormat('d F Y') }}</div>
                                 <div class="dropdown">
-                                    <a class="text-soft dropdown-toggle btn btn-icon btn-trigger"
-                                        data-bs-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
+                                    <a class="text-soft dropdown-toggle btn btn-icon btn-trigger" data-bs-toggle="dropdown">
+                                        <em class="icon ni ni-more-h"></em>
+                                    </a>
                                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-xs">
                                         <ul class="link-list-plain">
                                             <li><a href="{{ route('news.edit', $newsItem->slug) }}">Edit</a></li>
                                             <li>
                                                 <a href="#" class="delete-link">Hapus</a>
-                                                <form class="delete-form" action="{{ route('news.delete', $newsItem->slug) }}"
-                                                    method="POST" style="display: none;">
+                                                <form class="delete-form" action="{{ route('news.delete', $newsItem->slug) }}" method="POST" style="display: none;">
                                                     @method('DELETE')
                                                     @csrf
                                                 </form>
